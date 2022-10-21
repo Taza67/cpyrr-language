e1daa4efee622e76ca5eeac452a1e99550684e92
Taza67 <tazaakil67@gmail.com>
2022-10-21 10:10:00 +0200

    feat: libérer la mémoire

--- src/allocation.c
+++ src/allocation.c
@@ -33,3 +33,18 @@ void * reallocation_mem_peda(void **pt, size_t nobjets, size_t taille) {
     return pt_realloc;
 }
 
+void libere_mem(void * pt) {
+    void ** adr_pt = (void **) pt;
+    if ((*adr_pt) != NULL) free(*adr_pt);
+    *adr_pt = NULL;
+}
+
+void libere_mem_peda(void * *pt) {
+    if ((*pt) != NULL)
+        free(*pt);
+    *pt = NULL;
+}
+
+void libere(void * pt) {
+    free(pt);
+}
