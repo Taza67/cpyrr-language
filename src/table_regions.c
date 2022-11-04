20c2923be458e9fb64560e9a5b522fef77cca09a
Taza67 <tazaakil67@gmail.com>
2022-11-04 17:00:00 +0200

    feat: afficher les régions et leurs arbres

--- src/table_regions.c
+++ src/table_regions.c
@@ -14,7 +14,6 @@ element_table_regions table_regions[TAILLE_MAX_TABLE_REGIONS];      // Table des
 /****************************************************************************************************************/
 
 // Initialise la table des régions
-
 void init_table_regions() {
     // Initialisation
     for (int i = 0; i < TAILLE_MAX_TABLE_REGIONS; i++) {
@@ -29,4 +28,28 @@ void init_table_regions() {
 /****************************************************************************************************************/
 
 // Affiche la table des régions
+void affichage_table_regions() {
+    fprintf(yyout, "Affichage de la table des régions :\n\n");
+    for(int i = 0; i < TAILLE_MAX_TABLE_REGIONS; i++)
+        if (table_regions[i].NIS != -1)
+            fprintf(yyout, "Indice :\t%3d\t||\tTaille :\t%3d\t||\tNIS :\t%3d\t\n",
+                   i, table_regions[i].taille, table_regions[i].NIS);
+    fprintf(yyout, "\n\n");
+}
 
+// Affiche les arbres des régions de la table
+void affichage_regions_arbres() {
+    fprintf(yyout, "Affichage des arbres :\n\n");
+    for (int i = TAILLE_MAX_TABLE_REGIONS - 1; i >= 0; i--)
+        if (table_regions[i].NIS != -1) 
+            affichage_region(i);
+    fprintf(yyout, "\n\n");
+}
+
+// Affiche l'arbre d'une une région
+void affichage_region(int num_region) {
+    for (int i = 0; i < table_regions[num_region].NIS; i++)
+        fputs("    ", yyout);
+    fprintf(yyout, "[Région : %2d ]\n", num_region);
+    affichage_arbre(table_regions[num_region].arbre_abstrait, table_regions[num_region].NIS);
+}
